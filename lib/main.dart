f8f5db736c99f257551ff03a8b693bbb39703db8
Lucas <lucdow7@gmail.com>
2022-02-08 12:00:50 +0100

    added files for exhibtion map

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class ExampleStartPage extends StatelessWidget {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
