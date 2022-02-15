77e7fa4f10275dab2e2f8b094eea580be211e516
Thomas Eriksson Mueller <thomasem@kth.se>
2022-02-15 11:59:21 +0100

    Created generalized question text and answer buttons, also added some images

---
+++ lib/missions/answers.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// Button widget which works as answers to a given Q for missions
+class Answer extends StatelessWidget {
+  // What the answer will do when pressed
+  final Function pressAction;
+
+  final Color textColor;
+
+  Answer(this.pressAction, this.textColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(8),
+      child: ElevatedButton(
+        child: Text(
+          "This does not work",
+          style: TextStyle(color: textColor),
+        ),
+        // TODO: fix function pointer to _answerQ in main.dart
+        // Gives error at the moment
+        onPressed: null,
+        // onPressed: pressAction,
+      ),
+    );
+  }
+}
