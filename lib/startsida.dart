bc4aa85d513f27cf2d56137ec31570ff4e65cc88
antonschulz <anton.ph.schulz@gmail.com>
2022-02-24 15:25:06 +0100

    Added cleaner design and new background image

--- lib/startsida.dart
+++ lib/startsida.dart
@@ -10,47 +10,41 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
       body: Center(
         child: Stack(
           alignment: Alignment.topCenter,
           children: <Widget>[ // takes a container widget (background image) and a column widget with text + button
-            Container( //Container for the background
+            Container( //Container for the background  
               alignment: Alignment.center,
-              child: Image.network(
-                'https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg',
+              child: Image(
+                image: AssetImage('assets/images/startsida.png'),
                 height: 1000,
                 width: double.infinity,
                 fit: BoxFit.cover,
               ),
             ),
-            Column( //Column with text and button
-              mainAxisAlignment: MainAxisAlignment.center,
+            Container(
+            margin: EdgeInsets.only(top: 60.0, bottom: 50.0),
+            child: Column( //Column with text and button
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 Text(
-                  'Välkommen! Ert lagnamn är:',
-                  style: Theme.of(context).textTheme.headline1,
+                  'Välkomna $groupName!',
+                  style: TextStyle(fontSize: 65, fontWeight: FontWeight.bold, color: Color.fromRGBO(151,144,187,1)),
                 ),
-                Text(
-                  '$groupName',
-                  style: Theme.of(context).textTheme.headline2,
-                ),
+                Spacer(),
                 ElevatedButton(
                   style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all<Size>(Size(250,80)),
+                    backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(152, 180, 187, 1)),
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(
-                          color: Colors.red,
-                          width: 4.0,
-                        ),
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
                     ),
                   ),
                   child: Text('Börja här!',
-                      style: Theme.of(context).textTheme.headline1),
+                      style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
                   onPressed: () { //when pressed move to class "FirstRoute"
                     Navigator.push(
                       context,
@@ -59,6 +53,7 @@ class MyHomePage extends StatelessWidget {
                   },
                 ),
               ],
+            ),
             ),
           ],
         ),
