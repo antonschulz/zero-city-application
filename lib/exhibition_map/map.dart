355e2ca842193c1f6b87445eab127798f8b80dd2
Lucas <lucdow7@gmail.com>
2022-03-01 13:54:53 +0100

    Created mission button class

--- lib/exhibition_map/map.dart
+++ lib/exhibition_map/map.dart
@@ -9,14 +9,44 @@ class Mission {
   Mission(this.name, this.number);
 }
 
-class MissionButton  {
-    Mission mission;
-    double left;
-    double top;
-    double width;
-    double height;
+class MissionButton extends StatelessWidget {
 
-    MissionButton(this.mission, this.left, this.top, this.width, this.height);
+  MissionButton({
+    required this.mission,
+    required this.left,
+    required this.top,
+    required this.width,
+    required this.height,
+
+  }) : super(key: ObjectKey(mission));
+
+  final Mission mission;
+  final double left;
+  final double top;
+  final double width;
+  final double height;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: left,
+      top: top,
+      child: Container(
+        width: width,
+        height: height,
+        child: Text("Hello ${mission.name}"),
+        decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+                color: Colors.black,
+                width: 1
+            )
+        ),
+      ),
+    );
+  }
 }
 
 List<Mission> missions = [
@@ -24,7 +54,7 @@ List<Mission> missions = [
 ];
 
 List<MissionButton> missionButtons = [
-  MissionButton(missions[0], 0, 0, 100, 100),
+  MissionButton(mission: missions[0], left: 100.0, top: 0, width: 100.0, height: 100.0)
 ];
 
 
@@ -37,25 +67,7 @@ class ExhibitionMap extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [
-            for (MissionButton btn in missionButtons) Positioned(
-                left: btn.left,
-                top: btn.top,
-                child: Container(
-                  width: btn.width,
-                  height: btn.height,
-                  child: Text("Hello ${btn.mission.name}"),
-                  decoration: BoxDecoration(
-                      color: const Color(0xff7c94b6),
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 1
-                      )
-                  ),
-                ),
-              )
-        ],
+        children: missionButtons,
       )
     );
   }
