3e83fb4b1e15a435caba67d15ff5e44f1d861930
Thomas Eriksson Mueller <thomasem@kth.se>
2022-04-14 13:45:09 +0200

    Added margin to text types and renamed zone_title and zone_body widgets

--- lib/text_types/zone_body.dart
+++ lib/text_types/zone_body.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
-class MissionBody extends StatelessWidget {
+class ZoneBody extends StatelessWidget {
   late String text;
 
-  MissionBody(this.text, {Key? key}) : super(key: key);
+  ZoneBody(this.text, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(text,
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: Text(
+        text,
         textAlign: TextAlign.left,
         style: const TextStyle(
           fontSize: 16,
           color: Colors.blueAccent,
-        ));
+        ),
+      ),
+    );
   }
 }
