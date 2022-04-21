76fba86a663fa179f2c032244cd1438cf29cf067
Thomas Eriksson Mueller <thomasem@kth.se>
2022-04-21 18:07:29 +0200

    Update graphics in planning lab

--- lib/zones/planning_lab/planning_lab3.dart
+++ lib/zones/planning_lab/planning_lab3.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
 import 'package:zero_city/exhibition_map/map.dart';
 import 'package:zero_city/exhibition_map/map_provider.dart';
+import 'package:zero_city/state/planning_lab_state.dart';
 import 'package:zero_city/text_types/mission_body.dart';
 import 'package:zero_city/text_types/mission_title.dart';
+import 'package:zero_city/utils/Graphics.dart';
 
 class PlanningLab3 extends StatefulWidget {
   const PlanningLab3({Key? key}) : super(key: key);
@@ -13,10 +15,24 @@ class PlanningLab3 extends StatefulWidget {
 }
 
 class _PlanningLab3State extends State<PlanningLab3> {
+  // Fel svar snackbar
+  SnackBar snack = const SnackBar(
+    backgroundColor: Colors.redAccent,
+    content: Text(
+      "Fel svar!",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    duration: Duration(seconds: 7),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(241, 216, 234, 1),
+      backgroundColor: Graphics.PINK,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
@@ -29,19 +45,36 @@ class _PlanningLab3State extends State<PlanningLab3> {
           const SizedBox(
             height: 100,
           ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 400),
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: "En bil står stilla __% av tiden",
+              ),
+              keyboardType: TextInputType.number,
+              maxLength: 2,
+              onSubmitted: (String str) {
+                context.read<PlanningLabState>().submit3(str);
+              },
+            ),
+          ),
           ElevatedButton(
             onPressed: () {
-              context
-                  .read<ExhibitionMapProvider>()
-                  .setCompleteMission("Planning Lab");
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ExhibitionMap()),
-              );
+              if (context.read<PlanningLabState>().correct) {
+                context
+                    .read<ExhibitionMapProvider>()
+                    .setCompleteMission("Planning Lab");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ExhibitionMap()),
+                );
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(snack);
+              }
             },
             child: const Text("Vi har hittat svaret! (tillbaka till kartan)"),
             style: ElevatedButton.styleFrom(
-              primary: Colors.green,
+              primary: context.watch<PlanningLabState>().color,
             ),
           ),
         ],
